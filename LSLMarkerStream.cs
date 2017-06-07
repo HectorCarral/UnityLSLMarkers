05104ea7a0188519a7cbba448d84e7a5f22c5043
Héctor López Carral <hectorcarral@icloud.com>
2017-06-07 13:46:15 +0200

    Add files via upload

---
+++ LSLMarkerStream.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using LSL;
+
+namespace Assets.LSL4Unity.Scripts.Custom
+{
+    [HelpURL("https://github.com/xfleckx/LSL4Unity/wiki#using-a-marker-stream")]
+    public class LSLMarkerStream : MonoBehaviour
+    {
+        private const string unique_source_id = "D3F83BB699EB49AB94A9FA44B88882AB";
+
+        public string lslStreamName = "markers";
+        public string lslStreamType = "Markers";
+
+        private liblsl.StreamInfo lslStreamInfo;
+        private liblsl.StreamOutlet lslOutlet;
+        private int lslChannelCount = 1;
+
+        //Assuming that markers are never send in regular intervalls
+        private double nominal_srate = liblsl.IRREGULAR_RATE;
+
+        private const liblsl.channel_format_t lslChannelFormat = liblsl.channel_format_t.cf_int32;
+
+        private int[] sample;
+ 
+        void Awake()
+        {
+			DontDestroyOnLoad (gameObject);
+
+            sample = new int[lslChannelCount];
+
+            lslStreamInfo = new liblsl.StreamInfo(
+                                        lslStreamName,
+                                        lslStreamType,
+                                        lslChannelCount,
+                                        nominal_srate,
+                                        lslChannelFormat,
+                                        unique_source_id);
+            
+            lslOutlet = new liblsl.StreamOutlet(lslStreamInfo);
+        }
+
+        public void Write(int marker)
+        {
+            sample[0] = marker;
+            lslOutlet.push_sample(sample);
+        }
+
+        public void Write(int marker, double customTimeStamp)
+        {
+            sample[0] = marker;
+            lslOutlet.push_sample(sample, customTimeStamp);
+        }
+
+        public void Write(int marker, float customTimeStamp)
+        {
+            sample[0] = marker;
+            lslOutlet.push_sample(sample, customTimeStamp);
+        }
+
+        public void WriteBeforeFrameIsDisplayed(int marker)
+        {
+            StartCoroutine(WriteMarkerAfterImageIsRendered(marker));
+        }
+
+        IEnumerator WriteMarkerAfterImageIsRendered(int pendingMarker)
+        {
+            yield return new WaitForEndOfFrame();
+
+            Write(pendingMarker);
+
+            yield return null;
+        }
+
+    }
+}
---
+++ SendMarker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.LSL4Unity.Scripts.Custom;
+
+public class SendMarker : MonoBehaviour {
+
+	public LSLMarkerStream markerStream;
+
+	// Markers
+	public const int MARKER_TEST = 1;	// Test marker
+
+
+	// For avoiding sending the same marker twice in a row
+	private int lastMarker = -1;
+
+	public void Awake () {
+		DontDestroyOnLoad (gameObject);
+		//markerStream = GameObject.Find ("LabStreamingLayer").GetComponent<LSLMarkerStreamMidas> ();
+	}
+
+	public void Send (int marker) {
+		// Avoid sending the same marker twice in a row
+		if (marker != lastMarker) {
+			markerStream.Write(marker);
+			lastMarker = marker;
+			Debug.Log ("Sending marker: " + marker);
+		}
+	}
+}
